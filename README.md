6ce8b457d7c9f165104be4a6878c30778fa592eb
Chris Montes <zeuschris123@gmail.com>
2022-10-15 19:52:16 -0300

    update 1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# Poke World
+> Es una aplicación web donde puedes buscar pokemones de un JSON local del proyecto. Accediendo a su informacion, atributos y imagenes
+
+## ⚡ Lenguajes y Frameworks utilizados
+
+<p align="left"> <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://sass-lang.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/> </a> <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> </a> </p>
+
+## ☄ Interface
+![](images/Interface.gif)
---
+++ js/app.js
@@ -0,0 +1,91 @@
+let inputBusqueda = document.querySelector('#input-search')
+let btnSubmit = document.querySelector('#search-btn')
+let divResultado = document.querySelector('#resultado')
+let divOscuro = document.querySelector('#modoOscuro')
+let imageMain = document.querySelector('#image-main')
+let navDarkmode = document.querySelector('#nav-darkmode')
+
+fetch('/data/pokemons.json')
+    .then((resp) => resp.json())
+    .then((data) => {
+        const search = (poke) => {
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                const dataPoke = data.find((e) => e.name === poke)
+                dataPoke ? resolve (dataPoke) : reject (
+
+                    Swal.fire('No se encontraron resultados.')
+
+                    )
+            },3000)
+        })
+    }
+        
+            btnSubmit.addEventListener('click', (e) => {
+            const value = inputBusqueda.value.toLowerCase()
+            e.preventDefault()
+            divResultado.innerHTML = `<div class="container">
+                                            <div class="loader"></div>
+                                      </div>`
+            search(value)
+            .then((resp) => {
+                divResultado.innerHTML = `
+                    <div class="div-pokemon">
+                    <img src="${resp.img}" class="img-pokemon" alt="${resp.name}">
+                    <img src="${resp.font}" class="font-pokemon" alt="letra-pokemon">
+                    <p>Type: ${resp.type}<p>
+                    <p>Attack: ${resp.attk}<p>
+                    <p>Def: ${resp.def}<p>
+                    <p>Age: ${resp.age}<p>
+                    </div>
+                `
+        })
+            .catch(() => {
+                divResultado.innerHTML = `<strong>Busca nuevamente...<strong>`
+                setTimeout(() => {
+                divResultado.innerHTML = ``
+                },5000)
+            })
+        })  
+    })    
+
+divOscuro.addEventListener('click', (e) => {
+    let element = document.body
+    element.classList.toggle('dark-mode')
+    e.preventDefault()
+    if (divOscuro.innerHTML === 'Dark Mode'){
+        divOscuro.innerHTML = 'Light Mode'
+        Toastify({
+            text: "Dark Mode Active 🌙",
+            duration: 1200,
+            newWindow: true,
+            gravity: "top", 
+            position: "right", 
+            stopOnFocus: false,
+            style: {
+              background: "linear-gradient(to right, #ddf4, #000)",
+            },
+            onClick: function(){}
+          }).showToast();
+        }else{
+            divOscuro.innerHTML = 'Dark Mode'
+            Toastify({
+                text: "Light Mode Active ☀️",
+                duration: 1200,
+                newWindow: true,
+                gravity: "top", 
+                position: "right", 
+                stopOnFocus: false,
+                style: {
+                    background: "linear-gradient(to right, #ffbd7f, #000)",
+                },
+                onClick: function(){}
+            }).showToast();
+        }
+})
+
+imageMain.addEventListener('click', (e) => {
+    e.preventDefault()
+})
+
+
